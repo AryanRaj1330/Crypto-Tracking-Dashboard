5ee1ca8205093c50f0e922d687529756f42b2bd0
AryanRaj1330 <24je0890@iitism.ac.in>
2024-12-12 02:17:24 +0530

    dedicated pages for coins added

--- frontPage.js
+++ frontPage.js
@@ -1,157 +1,93 @@
-async function fetchPriceUSD(){
-    let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
+async function fetchCurrencyData(){
+  let currency=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
 
-    for(let code of coins){
-        await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
-            method:"POST",
-            headers: new Headers({
-              "content-type":"application/json",
-              "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
-            }),
-            body:JSON.stringify({
-              currency:"USD",
-              code:code,
-              meta:false,
-            }),
-          }).then(response=> response.json())
-            .then(data=>{
-                let price=document.getElementById(`${code}`)
-                price.textContent=data.rate.toFixed(2) 
-            })
-            .catch(error=>{
-              let usdError=document.getElementById(`${code}`)
-              usdError.textContent="Error"
-            });
-    }
-}
-
-fetchPriceUSD()
-
-async function fetchPriceINR(){
-    let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
-
-    for(let code of coins){
-        await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
-            method:"POST",
-            headers: new Headers({
-              "content-type":"application/json",
-              "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
-            }),
-            body: JSON.stringify({
-              currency:"INR",
-              code:code,
-              meta:false,
-            }),
-          }).then(response=> response.json())
-            .then(data=>{
-                let price=document.getElementById(`${code}inr`)
-                price.textContent=data.rate.toFixed(2) 
-            })
-            .catch(error=>{
-              let inrError=document.getElementById(`${code}inr`)
-              inrError.textContent="Error"
-            });
-    }
-}
-
-fetchPriceINR()
-
-
-async function fetchChange(){
-    let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
-
-    for(let code of coins){
-        await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
-            method:"POST",
-            headers: new Headers({
-              "content-type":"application/json",
-              "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
-            }),
-            body: JSON.stringify({
-              currency:"USD",
-              offset:0,
-              limit:10,
-              code:code,
-              meta:false,
-            }),
-          }).then(response=> response.json())
-            .then(data=>{
-                let delta=document.getElementById(`${code}delta`)
-                let deltaValue=data.delta.day
-                if(deltaValue<1){
-                    delta.style.color='red'
-                }
-                else if(deltaValue>=1){
-                    delta.style.color='green'
-                }
-                delta.textContent=data.delta.day.toFixed(2) 
-            })
-            .catch(error=>{
-              let changeError=document.getElementById(`${code}delta`)
-              changeError.textContent="Error"
-            });
-    }
-}
-
-fetchChange()
-
-async function fetchCap(){
-    let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
-
-    for(let code of coins){
-        await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
-            method:"POST",
-            headers: new Headers({
-              "content-type":"application/json",
-              "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
-            }),
-            body:JSON.stringify({
-              currency:"USD",
-              code:code,
-              meta:false,
-            }),
-          }).then(response=> response.json())
-            .then(data=>{
-                let capShow=document.getElementById(`${code}cap`)
-                let cap=data.cap.toFixed(2)
-                capShow.textContent=cap
-            })
-            .catch(error=>{
-              let capError=document.getElementById(`${code}cap`)
-              capError.textContent="Error"
-            });
-    }
-}
-
-fetchCap()
-
-async function fetchVolume(){
-  let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
-
-  for(let code of coins){
-      await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
-          method:"POST",
-          headers: new Headers({
-            "content-type":"application/json",
-            "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
-          }),
-          body:JSON.stringify({
-            currency:"USD",
-            code:code,
-            meta:false,
-          }),
-        }).then(response=> response.json())
-          .then(data=>{
-            console.log(data.volume)
-              let volume=data.volume
-              let volumeShow=document.getElementById(`${code}volume`)
-              volumeShow.textContent=volume
+  for(let coin of currency){
+      try{
+          let response= await fetch(new Request("https://api.livecoinwatch.com/coins/single"),{
+              method:"POST",
+              headers: new Headers({
+                  "content-type":"application/json",
+                  "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3"
+              }),
+              body:JSON.stringify({
+                  currency:"USD",
+                  code:coin,
+                  sort:"rank",
+                  order:"ascending",
+                  offset:0,
+                  limit:10,
+                  meta:false
+              })
           })
-          .catch(error=>{
-            let errorVolume=document.getElementById(`${code}volume`)
-            errorVolume.textContent="Error"
-          });
+
+          let data= await response.json()
+
+          let priceUSD=document.getElementById(coin)
+          priceUSD.textContent=data.rate.toFixed(2)
+
+          let cap=document.getElementById(`${coin}cap`)
+          cap.textContent=data.cap
+
+          let volume=document.getElementById(`${coin}volume`)
+          volume.textContent=data.volume
+
+          let delta=document.getElementById(`${coin}delta`)
+          let change=data.delta.day
+          if(change<1){
+              delta.style.color='red'
+          }
+          else if(change>=1){
+              delta.style.color='green'
+          }
+          delta.textContent=change
+      }
+      catch(error){
+        console.log(error)
+
+          priceUSD.textContent="Error"
+          cap.textContent="Error"
+          volume.textContent="Error"
+          delta.textContent="Error"
+      }
   }
 }
 
-fetchVolume()
+fetchCurrencyData()
+
+async function fetchPriceInr(){
+  let currency=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
+
+  for(let coin of currency){
+      try{
+          let responseInr= await fetch(("https://api.livecoinwatch.com/coins/single"),{
+              method:"POST",
+              headers: new Headers({
+                  "content-type":"application/json",
+                  "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3"
+              }),
+              body:JSON.stringify({
+                  currency:"INR",
+                  code:coin,
+                  sort:"rank",
+                  order:"ascending",
+                  offset:0,
+                  limit:10,
+                  meta:false
+              })
+          })
+      
+          let dataInr= await responseInr.json()
+      
+          let priceINR=document.getElementById(`${coin}inr`)
+          priceINR.textContent=dataInr.rate.toFixed(2)
+      }
+
+      catch(error){
+        console.log(error)
+
+          priceINR.textContent="Error"
+      }
+  }
+}
+
+fetchPriceInr()
---
+++ frontPageTest.js
@@ -0,0 +1,165 @@
+async function fetchPriceUSD(){
+    let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
+
+    for(let code of coins){
+        await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
+            method:"POST",
+            headers: new Headers({
+              "content-type":"application/json",
+              "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
+            }),
+            body:JSON.stringify({
+              currency:"USD",
+              offset:0,
+              limit:10,
+              code:code,
+              meta:false,
+            }),
+          }).then(response=> response.json())
+            .then(data=>{
+                let price=document.getElementById(`${code}`)
+                price.textContent=data.rate.toFixed(2) 
+            })
+            .catch(error=>{
+              let usdError=document.getElementById(`${code}`)
+              usdError.textContent="Error"
+            });
+    }
+}
+
+fetchPriceUSD()
+
+async function fetchPriceINR(){
+    let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
+
+    for(let code of coins){
+        await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
+            method:"POST",
+            headers: new Headers({
+              "content-type":"application/json",
+              "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
+            }),
+            body: JSON.stringify({
+              currency:"INR",
+              offset:0,
+              limit:10,
+              code:code,
+              meta:false,
+            }),
+          }).then(response=> response.json())
+            .then(data=>{
+                let price=document.getElementById(`${code}inr`)
+                price.textContent=data.rate.toFixed(2) 
+            })
+            .catch(error=>{
+              let inrError=document.getElementById(`${code}inr`)
+              inrError.textContent="Error"
+            });
+    }
+}
+
+fetchPriceINR()
+
+
+async function fetchChange(){
+    let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
+
+    for(let code of coins){
+        await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
+            method:"POST",
+            headers: new Headers({
+              "content-type":"application/json",
+              "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
+            }),
+            body: JSON.stringify({
+              currency:"USD",
+              offset:0,
+              limit:10,
+              code:code,
+              meta:false,
+            }),
+          }).then(response=> response.json())
+            .then(data=>{
+                let delta=document.getElementById(`${code}delta`)
+                let deltaValue=data.delta.day
+                if(deltaValue<1){
+                    delta.style.color='red'
+                }
+                else if(deltaValue>=1){
+                    delta.style.color='green'
+                }
+                delta.textContent=data.delta.day.toFixed(2) 
+            })
+            .catch(error=>{
+              let changeError=document.getElementById(`${code}delta`)
+              changeError.textContent="Error"
+            });
+    }
+}
+
+fetchChange()
+
+async function fetchCap(){
+    let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
+
+    for(let code of coins){
+        await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
+            method:"POST",
+            headers: new Headers({
+              "content-type":"application/json",
+              "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
+            }),
+            body:JSON.stringify({
+              currency:"USD",
+              offset:0,
+              limit:10,
+              code:code,
+              meta:false,
+            }),
+          }).then(response=> response.json())
+            .then(data=>{
+                let capShow=document.getElementById(`${code}cap`)
+                let cap=data.cap.toFixed(2)
+                capShow.textContent=cap
+            })
+            .catch(error=>{
+              let capError=document.getElementById(`${code}cap`)
+              capError.textContent="Error"
+            });
+    }
+}
+
+fetchCap()
+
+async function fetchVolume(){
+  let coins=["BTC","ETH","XRP","ADA","XLM","LTC","NEO","DOT","TRX","LINK"]
+
+  for(let code of coins){
+      await fetch(new Request("https://api.livecoinwatch.com/coins/single"), {
+          method:"POST",
+          headers: new Headers({
+            "content-type":"application/json",
+            "x-api-key":"cb9a09fe-f9f3-40b7-9c38-4883cf04ecf3",
+          }),
+          body:JSON.stringify({
+            currency:"USD",
+            offset:0,
+              limit:10,
+            code:code,
+            meta:false,
+          }),
+        }).then(response=> response.json())
+          .then(data=>{
+            console.log(data.volume)
+              let volume=data.volume
+              let volumeShow=document.getElementById(`${code}volume`)
+              volumeShow.textContent=volume
+          })
+          .catch(error=>{
+            let errorVolume=document.getElementById(`${code}volume`)
+            errorVolume.textContent="Error"
+          });
+  }
+}
+
+fetchVolume()
