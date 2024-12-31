9e124e61b9698dddccf25b593d5978e94833b6b3
AryanRaj1330 <24je0890@iitism.ac.in>
2025-01-01 00:29:56 +0530

    dheere dheere kuch improvement but still not 10%

--- metaMask/login.js
+++ metaMask/login.js
@@ -1,6 +1,43 @@
-let logBtn=document.getElementById("login-button")
+let logBtn=document.getElementById("login")
 
-if(!window.ethereum){
-    logBtn.innerHTML="MetaMask not installed"
-    logBtn.style.backgroundColor="red"
+const MMSDK= new MetaMaskSDK.MetaMaskSDK({
+    dappMetadata:{
+        name:"CryptoSpy Dapp",
+    },
+    infuraAPIKey:"1a9bfff02ceb447e88d9a54a673b2392",
+})
+
+console.log("MetaMask SDK initialized")
+
+function toggle(){
+    if(!window.ethereum){
+        logBtn.innerHTML="MetaMask not installed"
+        logBtn.style.backgroundColor="red"
+        logBtn.classList.add("hover")
+        document.getElementById("not-message").innerText="Please install MetaMask to continue"
+        console.log("MetaMask not installed")
+    }
+
+    else{
+        logBtn.addEventListener("click",()=>{
+            loginMetaMask()
+        })
+    }
 }
+
+async function loginMetaMask(){
+    try{
+    const account= await window.ethereum.request({method:"eth_requestAccounts"})
+    console.log("success")
+    window.location.href="../index.html"
+    }
+    catch(error){
+        console.log(`error: ${error}`)
+    }
+}
+
+window.addEventListener("DOMContentLoaded",()=>{
+    logBtn.addEventListener("click",()=>{
+        toggle()
+    })
+})
